511bc2451febcb67eab489c638c16adb20fbacfc
João Pedro Lazarim de Souza <jplazarim@gmail.com>
2018-07-12 16:33:47 -0300

    Add files via upload

--- calculadora_redes/functions.php
+++ calculadora_redes/functions.php
@@ -1,121 +1,83 @@
 <?php
-
-    require_once "index.php";
-
-
-    function calc ($ipInformado, $mask){
+        function calc ($ipInformado, $mask){
 
 
-        $enderecos = pow(2,(32-$mask));
-        $subredes = 256 / $enderecos;
-        $hosts = $subredes - 2;
-        $ip = explode("." , "$ipInformado");
+            $enderecos = floor(pow(2,(32-$mask)));
+            $subredes = floor(256 / $enderecos);
+            $hosts = $subredes - 2;
+            $ip = explode("." , "$ipInformado");
+            $a = $ip[3] / $enderecos;
+            $firstHost = floor($a) * $enderecos + 1;
+            $lastHost = $firstHost + ($enderecos - 3);
+            $broadcast = $lastHost + 1;
+            $maskDecimal = 256 - $enderecos;
 
-        $a = $ip[3] / $enderecos;
-        $firstHost = floor($a) * $enderecos + 1;
-        $lastHost = $firstHost + ($enderecos - 3);
-        $broadcast = $lastHost + 1;
-        $maskDecimal = 256 - $enderecos;
+            if ($hosts < 0) {
+            	$hosts = 0;
+            }
 
-        switch ($ipInformado){
-            case $ipInformado >= 0 AND $ipInformado <= 127:
-                $classe =  "classe A";
-                break;
+            if ($subredes < 0) {
+            	$subredes = 0;
+            }
 
-            case $ipInformado >= 128 AND $ipInformado <= 191:
-                $classe = "classe B";
-                break;
 
-            case $ipInformado >=192 AND $ipInformado <= 223:
-                $classe = "classe C";
-                break;
+            switch ($ipInformado){
+                case $ipInformado >= 0 AND $ipInformado <= 127:
+                    $classe =  "Classe A";
+                    break;
 
-            case $ipInformado >= 240 AND $ipInformado <= 255:
-                $classe = "classe D";
-                break;
+                case $ipInformado >= 128 AND $ipInformado <= 191:
+                    $classe = "Classe B";
+                    break;
+
+                case $ipInformado >=192 AND $ipInformado <= 223:
+                    $classe = "Classe C";
+                    break;
+
+                case $ipInformado >= 240 AND $ipInformado <= 255:
+                    $classe = "Classe D";
+                    break;
+            }
+
+            switch ($ipInformado){
+                case $ipInformado >= 10 AND $ipInformado <= 10.255:
+                    $classeIp =  "Privado";
+                    break;
+
+                case $ipInformado >= 172.16 AND $ipInformado <= 172.31:
+                    $classeIp = "Privado";
+                    break;
+
+                case $ipInformado >=192.168 AND $ipInformado <= 172.169:
+                    $classeIp = "Privado";
+                    break;
+
+                default:
+                    $classeIp = "Público";
+                    break;
+            }
+
+
+            $ar = ['enderecos' => $enderecos,
+                'subredes' => $subredes,
+                'hosts' => $hosts,
+                'primeiro endereco de host' => "$ip[0].$ip[1].$ip[2].$firstHost",
+                'ultimo endereco de host' => "$ip[0].$ip[1].$ip[2].$lastHost",
+                'broadcast' => "$ip[0].$ip[1].$ip[2].$broadcast",
+                'mascara de rede' => "255.255.255.$maskDecimal",
+                'classe do ip' => $classe,
+                'tipo do ip' => $classeIp
+            ];
+
+            return $ar;
+
         }
 
-        switch ($ipInformado){
-            case $ipInformado >= 10 AND $ipInformado <= 10.255:
-                $classeIp =  "privado";
-                break;
-
-            case $ipInformado >= 172.16 AND $ipInformado <= 172.31:
-                $classeIp = "privado";
-                break;
-
-            case $ipInformado >=192.168 AND $ipInformado <= 172.169:
-                $classeIp = "privado";
-                break;
-
-            default:
-                $classeIp = "publico";
-                break;
-        }
-
-
-        $ar = ['enderecos' => $enderecos,
-            'subredes' => $subredes,
-            'hosts' => $hosts,
-            'primeiro endereco de host' => "$ip[0].$ip[1].$ip[2].$firstHost",
-            'ultimo endereco de host' => "$ip[0].$ip[1].$ip[2].$lastHost",
-            'broadcast' => "$ip[0].$ip[1].$ip[2].$broadcast",
-            'mascara de rede' => "255.255.255.$maskDecimal",
-            'classe do ip' => "$classe, $classeIp"
-        ];
-
-
-
-        print_r($ar);
-
-//        if ($mask == 24) {
-//            echo "O IP contém 1 subrede\n";
-//            echo "Cada subrede contém 256 endereços";
-//        }
-//
-//        elseif ($mask == 25) {
-//            echo "O IP contém 2 subredes\n";
-//            echo "Cada subrede contém 128 endereços";
-//        }
-//
-//        elseif ($mask == 26) {
-//            echo "O IP contém 4 subredes\n";
-//            echo "Cada subrede contém 64 endereços";
-//        }
-//
-//        elseif ($mask == 27) {
-//            echo "O IP contém 8 subredes\n";
-//            echo "Cada subrede contém 32 endereços";
-//        }
-//
-//        elseif ($mask == 28) {
-//            echo "O IP contém 16 subredes\n";
-//            echo "Cada subrede contém 16 endereços";
-//        }
-//
-//        elseif ($mask == 29) {
-//            echo "O IP contém 32 subredes\n";
-//            echo "Cada subrede contém 8 endereços";
-//        }
-//
-//        elseif ($mask == 30) {
-//            echo "O IP contém 64 subredes\n";
-//            echo "Cada subrede contém 4 endereços";
-//        }
-//
-//        elseif ($mask == 31) {
-//            echo "O IP contém 128 subredes\n";
-//            echo "Cada subrede contém 2 endereços";
-//        }
-//
-//        elseif ($mask == 32) {
-//            echo "O IP contém 256 subredes\n";
-//            echo "Cada subrede contém 1 endereço";
-//        }
+    if (isset($_GET['ip'])){
+        $ip = $_GET['ip'];
+        $mask = $_GET['mask'];
+        $calculo = json_encode(calc($ip, $mask));
+        header("Conten-type: application/json");
+        echo $calculo;
     }
 
-//    $ip = $_POST['ip'];
-//    $mask = $_POST['mask'];
-    $ip = '192.168.10.156';
-    $mask = '27';
-    $calculo = calc($ip, $mask);
